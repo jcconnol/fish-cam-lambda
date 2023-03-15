1a955610deb2a6fae830795cdf56a1e9d7be639d
John Connolly <jcconnol4@gmail.com>
2023-03-14 19:54:15 -0500

    fix s3 retrieval

--- index.py
+++ index.py
@@ -4,8 +4,8 @@ from responses import response
 import boto3
 
 client = boto3.client('iot-data', region_name='us-east-2')
-BUCKET_NAME = "fish_cam"
-FISH_DATA_JSON_FILE_NAME = "fish_data.json"
+BUCKET_NAME = "fish-cam"
+FISH_DATA_JSON_FILE_NAME = "fish-cam.json"
 FISH_DATA_JPG_IMAGE_NAME = "fish_pic.jpg"
 
 
@@ -18,19 +18,16 @@ def endpoint(event, context):
             payload=b"{\"message\": \"invoke fish cam!\"}"
         )
         print(response)
-        print("got here")
-        
-        bucket = "fish-cam"
-        filename = "fish_data.json"
         
         unsigned_url = boto3.client('s3').generate_presigned_url(
-        ClientMethod='get_object', 
-        Params={'Bucket': BUCKET_NAME, 'Key': FISH_DATA_JPG_IMAGE_NAME}, ExpiresIn=3600)
-            
-        s3 = boto3.resource('s3')
-        fish_cam_content = s3.Object(bucket, filename)
-        fish_cam_json = fish_cam_content.get()['Body'].read().decode('utf-8') 
+            ClientMethod='get_object', 
+            Params={'Bucket': BUCKET_NAME, 'Key': FISH_DATA_JPG_IMAGE_NAME}, ExpiresIn=3600
+        )
         
+        s3 = boto3.resource('s3')
+        fish_cam_content = s3.Object(BUCKET_NAME, FISH_DATA_JSON_FILE_NAME)
+        fish_cam_data = fish_cam_content.get()['Body'].read().decode('utf-8')
+        fish_cam_json = json.loads(fish_cam_data)
         fish_cam_json["s3_url"] = unsigned_url
 
         return {
